7c269791df8f2e51be4a1786005215086ccd06a1
Carlosmr-10 <carlosmichelenar.sanjosemlg@fundacionloyola.net>
2026-02-13 13:18:13 +0100

    12345678op

--- ProyectoRepositorio1/src/paqueterepositorio1/ClaseRepositorio1.java
+++ ProyectoRepositorio1/src/paqueterepositorio1/ClaseRepositorio1.java
@@ -1,8 +1,17 @@
 package paqueterepositorio1;
 
 public class ClaseRepositorio1 {
+	
+	public ClaseRepositorio1() {
+		
+	}
 
 	private void Ennove() {
 		System.out.println("Ennove");
+	}
+	
+	private void pruebaMerge() {
+		
+		System.out.println("prueba merge");
 	}
 }
